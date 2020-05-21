aa04512f45237957974f2e8f2f2623a71f6e8abe
Lenny P Critchley <doaltplusf4@gmail.com>
2020-05-21 16:06:32 +0100

    Added basic player.

--- main.py
+++ main.py
@@ -1,26 +1,69 @@
 import pyglet
-
-window = pyglet.window.Window(caption="Test", resizable=True)
-
-label = pyglet.text.Label(
-    text="Hello World!",
-    x=window.width//2,
-    y=window.height//2,
-    anchor_x="center",
-    anchor_y="center"
-)
+from pyglet.window import key
 
 
-@window.event
-def on_draw():
-    window.clear()
-    label.draw()
+class Player:
+    def __init__(self, application):
+        self.application = application
+
+        self.sprite = pyglet.shapes.Circle(
+            self.application.window.width//2,
+            self.application.window.height//2,
+            radius=50
+        )
+
+        self.application.window.push_handlers(self)
+
+    def update(self, dt):
+        SPEED = 250
+
+        if (
+            (
+                self.application.key_handler[key.UP] ^
+                self.application.key_handler[key.DOWN]
+            ) and (
+                self.application.key_handler[key.LEFT] ^
+                self.application.key_handler[key.RIGHT]
+            )
+        ):
+            SPEED /= 2**0.5
+
+        if self.application.key_handler[key.UP]:
+            self.sprite.y += SPEED*dt
+        if self.application.key_handler[key.DOWN]:
+            self.sprite.y -= SPEED*dt
+        if self.application.key_handler[key.LEFT]:
+            self.sprite.x -= SPEED*dt
+        if self.application.key_handler[key.RIGHT]:
+            self.sprite.x += SPEED*dt
+
+    def draw(self):
+        self.sprite.draw()
 
 
-@window.event
-def on_resize(width, height):
-    label.x = width//2
-    label.y = height//2
+class Application:
+    def __init__(self):
+        self.window = pyglet.window.Window(caption="Test", resizable=True)
+
+        self.key_handler = key.KeyStateHandler()
+        self.window.push_handlers(self.key_handler)
+
+        self.player = Player(self)
+
+        self.window.push_handlers(self)
+
+    def update(self, dt):
+        self.player.update(dt)
+
+    def on_draw(self):
+        self.window.clear()
+        self.player.draw()
+
+    def run(self):
+        pyglet.clock.schedule_interval(self.update, 1/60)
+        pyglet.app.run()
 
 
-pyglet.app.run()
+if __name__ == "__main__":
+    application = Application()
+    application.run()
